836fc05ba2e5da3d759a6d1342b889f55bde2bd3
VonFriedricht <39649581+VonFriedricht@users.noreply.github.com>
2019-02-03 21:30:32 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,20 +3,14 @@ const bot = new Discord.Client()
 
 const CommitGuide = require("./CommitGuide.js")
 const commitguide = new CommitGuide(bot, {
-    top_left_day: TOP_LEFT_DAY,
-    target_image: COMMIT_IMAGE,
-    lyrics: LYRICS
+    top_left_day: process.env.top_left_day,
+    target_image: process.env.commit_image,
+    lyrics: process.env.lyrics,
+    admin: process.env.admin
 })
 
-const ADMIN = process.env.admin
-const TOP_LEFT_DAY = process.env.top_left_day
-const COMMIT_IMAGE = process.env.commit_image
-const LYRICS = process.env.lyrics
-const DESTINY_TOKEN = process.env.destiny
-
 bot.on("ready",async function(){
-    bot.admin = bot.users.find(user=>user.id==ADMIN)
     console.log("My Body is Ready!")
 })
 
-bot.login(DESTINY_TOKEN)
+bot.login(process.env.destiny)

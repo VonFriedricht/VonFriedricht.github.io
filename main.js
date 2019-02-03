efd6733b0da295202b96bdff406569c111d87c51
VonFriedricht <39649581+VonFriedricht@users.noreply.github.com>
2019-02-03 21:57:07 +0100

    Update main.js

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ const CommitGuide = require("./CommitGuide.js")
 const commitguide = new CommitGuide(bot, {
     top_left_day: process.env.top_left_day,
     target_image: process.env.commit_image,
-    lyrics: process.env.lyrics,
-    admin: process.env.admin.split(";")
+    lyrics: process.env.lyrics.split("+"),
+    admin: process.env.admin
 })
 
 bot.on("ready",async function(){

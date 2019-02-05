d84787fca99e25f45acd992cf99c2f8bc4736b40
VonFriedricht <39649581+VonFriedricht@users.noreply.github.com>
2019-02-05 16:05:08 +0100

    and

---
+++ CommitGuide.js
@@ -0,0 +1,172 @@
+/**
+ * helping helper for the commits to make it an image ¯\_(ツ)_/¯
+ */
+class CommitGuide{
+    constructor(bot, options){
+        this.preview_tiles = options.preview_tiles || "─░▓█"
+        this.top_left_day = options.top_left_day || "1970-01-01"
+        this.target_image = options.target_image || "1111111122222413333344444444"
+        this.lyrics = options.lyrics || false
+        this.tile_sizes = options.tile_sizes || [0,1,5,10]
+        this.admin = options.admin || -1
+                
+        bot.on("ready", () => {
+            bot.admin = bot.users.find(user=>user.id==this.admin)
+            this.on_ready(bot)
+        })
+        bot.on("message", (message) => {
+            this.on_message(message)
+        })
+    }
+    
+    on_ready(bot){
+        console.log(this)
+        if( bot.admin ){
+            bot.admin.send("booted")
+            this.remind(bot.admin, 5)
+        }
+    }
+    
+    on_message(message){
+        if(message.content == "test"){
+            message.reply(this.preview_tiles)
+        }
+        if(message.content == ".preview"){
+            message.channel.send(this.preview)
+        }
+        if(message.content == ".now"){
+            var date = this.current_date
+            var target = this.current_tile
+            var preview = this.preview_tiles[target-1]
+            var size = this.tile_sizes[target-1]
+            var lyrics_snippet = this.current_lyrics
+
+            message.channel.send(JSON.stringify({date, target, preview, size, lyrics_snippet}))
+        }
+        if(message.content.startsWith(".phrases of ")){
+            var parameter = message.content.substring(".phrases of ".length)
+            message.channel.send(parameter)
+        }
+    }
+    
+    remind(user, time){
+        user.send("today's commit-names: ")
+        user.send(this.current_lyrics.join("\n"))
+    }
+
+   /**
+    * tiles for the target_image-preview at the command .preview
+    * @param {string|array} new_value the tiles for the preview
+    */
+    set preview_tiles(new_value){
+        if(typeof new_value[0] != "undefined"){
+            this._preview_tiles = new_value
+        }
+        else{
+            console.error("preview_tiles has to be indexable")
+        }
+    }
+    get preview_tiles(){
+        return this._preview_tiles
+    }
+    
+   /**
+    * the date for the very first tile in the collection 
+    * (should be the one on the top right)
+    * @param {string|Date} time the date of the first tile
+    */
+    set top_left_day(time){
+        if(new Date(time).getTime()){
+            this._top_left_day = new Date(time)
+        }
+        else{
+            console.error("top_left_day can't be an Invalid Time")
+        }
+    }
+    get top_left_day(){
+        return this._top_left_day
+    }
+    
+   /**
+    * setting the target_image for the commits
+    * @param {string|array} new_value the new target_image
+    */ 
+    set target_image(new_value){
+        if(typeof new_value[0] != "undefined"){
+            this._target_image = new_value
+        }
+        else{
+            console.error("target_image has to be some indexable")
+        }
+    }
+    get target_image(){
+        return this._target_image
+    }
+    
+   /**
+    * guiding thread for the commit-names
+    * @param {string|array} new_value the new lyrics (string gets space-seperated by default)
+    */
+    set lyrics(new_value){
+        if(typeof new_value == "string"){
+            new_value = new_value.split(",")
+        }
+        this._lyrics = new_value.filter(element=>element.length>0)
+    }
+    get lyrics(){
+        return this._lyrics
+    }
+    
+   /**
+    * the size of each tile in commits
+    * @param {string|array} new_value the new tile-sizes
+    */
+    set tile_sizes(new_value){
+        this._tile_sizes = new_value
+    }
+    get tile_sizes(){
+        return this._tile_sizes
+    }
+
+
+    // get current_tile
+    get current_tile(){
+        return this.target_image[this.current_date]
+    }
+
+   /**
+    * the current tile relative to the target_image-beginning
+    */
+    get current_date(){
+        var time_ms = new Date().getTime()-this.top_left_day.getTime()
+        var time_days = time_ms/1000/60/60/24
+        return Math.floor(time_days)
+    }
+    
+   /**
+    * getting the target_image translated into a preview
+    */
+    get preview(){
+        var result = new Array(7).fill("")
+        for(var i in this.target_image){
+            var tile = this.preview_tiles[this.target_image[i]-1]
+            result[(i%7)] += tile+tile
+        }
+        return result.join("\n")
+    }
+
+   /**
+    * getting the current relevant lyrics-snippet
+    */
+    get current_lyrics(){
+        var offset = 0
+        this.target_image.substr(0,this.current_date).split("").forEach(tile=>{
+            console.log(offset, tile)
+            offset += parseInt(this.tile_sizes[parseInt(tile)-1])
+        })
+        var current_size = this.tile_sizes[this.current_tile-1]
+        return this.lyrics.slice(offset-1,offset+current_size-1)
+    }
+}
+
+module.exports = CommitGuide
--- main.js
+++ main.js
@@ -1,123 +1,16 @@
-Discord = require("discord.js")
+const Discord = require("discord.js")
 const bot = new Discord.Client()
 
-const ADMIN = process.env.admin
-const TOP_LEFT_DAY = process.env.top_left_day
-const COMMIT_IMAGE = process.env.commit_image
-const LYRICS = process.env.lyrics.split("+")
-const DESTINY_TOKEN = process.env.destiny
-
-bot.on("ready",async function(){
-    bot.admin = bot.users.find(user=>user.id==ADMIN)
-    console.log("My Body is Ready!")
-    const commitguide = new CommitGuide(bot, {
-        top_left_day: TOP_LEFT_DAY,
-        target_image: COMMIT_IMAGE,
-        lyrics: LYRICS
-    })
+const CommitGuide = require("./CommitGuide.js")
+const commitguide = new CommitGuide(bot, {
+    top_left_day: process.env.top_left_day,
+    target_image: process.env.commit_image,
+    lyrics: process.env.lyrics.split(" "),
+    admin: process.env.admin
 })
 
-bot.login(DESTINY_TOKEN)
+bot.on("ready",async function(){
+    console.log("My Body is Ready!")
+})
 
-class CommitGuide{
-    constructor(bot, options){
-        this.preview_tiles = options.preview_tiles || "─░▓█"
-        this.top_left_day = options.top_left_day || "1970-01-01"
-        this.target_image = options.target_image || "1111111122222413333344444444"
-        this.lyrics = options.lyrics || false
-        this.tile_sizes = options.tile_sizes || [0,1,5,10]
-        
-        bot.on("message", (message) => {
-            this.on_message(message)
-        })
-    }
-    
-    set preview_tiles(new_value){
-        if(typeof new_value[0] != "undefined"){
-            this._preview_tiles = new_value
-        }
-        else{
-            console.error("preview_tiles has to be indexable")
-        }
-    }
-    get preview_tiles(){
-        return this._preview_tiles
-    }
-    
-    set top_left_day(time){
-        if(new Date(time).getTime()){
-            this._top_left_day = new Date(time)
-        }
-        else{
-            console.error("top_left_day can't be an Invalid Time")
-        }
-    }
-    get top_left_day(){
-        return this._top_left_day
-    }
-    
-    set target_image(new_value){
-        this._target_image = new_value
-    }
-    get target_image(){
-        return this._target_image
-    }
-    
-    set lyrics(new_value){
-        if(typeof new_value == "string"){
-            new_value = new_value.split(" ")
-        }
-        this._lyrics = new_value
-    }
-    get lyrics(){
-        return this._lyrics
-    }
-    
-    set tile_sizes(new_value){
-        this._tile_sizes = new_value
-    }
-    get tile_sizes(){
-        return this._tile_sizes
-    }
-    
-    get current_date(){
-        var time_ms = new Date().getTime()-this.top_left_day.getTime()
-        var time_days = time_ms/1000/60/60/24
-        return Math.floor(time_days)
-    }
-    
-    set target_image(new_value){
-        if(typeof new_value[0] != "undefined"){
-            this._target_image = new_value
-        }
-        else{
-            console.error("target_image has to be some indexable")
-        }
-    }
-    
-    get preview(){
-        var result = new Array(7).fill("")
-        for( var i in this.target_image ){
-            var tile = this.preview_tiles[this.target_image[i]-1]
-            result[(i%7)] += tile+tile
-        }
-        return result.join("\n")
-    }
-    
-    on_message(message){
-        console.log(this)
-        if(message.content == "test"){
-            message.reply(this.preview_tiles)
-        }
-        if(message.content == ".preview"){
-            message.channel.send(this.preview)
-        }
-        if(message.content == ".now"){
-            message.channel.send(this.current_date)
-        }
-        if(message.content.startsWith(".phrases of ")){
-            var parameter = message.content.substring(".phrases of ".length)
-            message.channel.send(parameter)
-        }
-    }
-}
+bot.login(process.env.destiny)

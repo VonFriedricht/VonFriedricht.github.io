6fddf1f8ab75368954ded5ce7feea20195d5c83a
VonFriedricht <39649581+VonFriedricht@users.noreply.github.com>
2019-02-07 22:26:29 +0100

    cries

--- CommitGuide/CommitGuide.js
+++ CommitGuide/CommitGuide.js
@@ -8,14 +8,5 @@ class CommitGuide{
     this.lyrics = options.lyrics || false
     this.tile_sizes = options.tile_sizes || [0,1,5,10]
     this.admin = options.admin || -1
-              
-    this.bot.on("ready", () => {
-        bot.admin = bot.users.find(user=>user.id==this.admin)
-        this.on_ready(bot)
-        console.log("CommitGuide Ready!")
-    })
-    this.bot.on("message", (message) => {
-      this.on_message(message)
-    })
   }
 }

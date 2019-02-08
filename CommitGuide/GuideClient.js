3dca1ebd4b5d2aff83018188b0edf905080b6263
VonFriedricht <39649581+VonFriedricht@users.noreply.github.com>
2019-02-08 21:12:24 +0100

    me?

--- CommitGuide/GuideClient.js
+++ CommitGuide/GuideClient.js
@@ -2,26 +2,36 @@ const Discord = require("discord.js")
 const Client = Discord.Client
 
 class GuideClient extends Client{
-  constructor(admin_id){
-    super()
-    this.on("ready",function(){
-        console.log(this)
-        this.admin = admin_id
-        this.admin.send("hi!")
-    })
-  }
-  set admin(new_admin){
-    if( typeof new_admin == "string" ){
-      this._admin = this.users.find(u=>u.id==new_admin)
+    constructor(){
+      super()
+      this.commands = []
     }
-    if( new_admin instanceof Discord.User ){
-      this._admin = new_admin
+    set admin(new_admin){
+        if( typeof new_admin == "string" ){
+            this._admin = this.users.find(u=>u.id==new_admin)
+        }
+        if( new_admin instanceof Discord.User ){
+             this._admin = new_admin
+        }
+      }
+    get admin(){
+        if( this._admin ){
+            return this._admin
+        }
     }
-  }
-  get admin(){
-    return this._admin
-  }
+    
+    add_command(name, func){
+        var command = new Command(name, func)
+        this.commands.push(command)
+    }
+    exec_command(message){
+        var command = message.content.toLowerCase().split(" ")[0]
+        var target_command = this.commands.find(c=>c.name==command)
+        if( target_command ){
+            target_command.execute(this, message)
+        }
+    }
+    listen(){
+        this.on("message",this.exec_command)
+    }
 }
-
-var client = new GuideClient("397063436049186818")
-client.login("ayyyy lmao")

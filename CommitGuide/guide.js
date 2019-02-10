9260eecef502ef4ec847a20723a07ae128547283
VonFriedricht <39649581+VonFriedricht@users.noreply.github.com>
2019-02-10 11:28:41 +0100

    Update guide.js

--- CommitGuide/guide.js
+++ CommitGuide/guide.js
@@ -1,10 +1,10 @@
 const CommitGuide = require("./CommitGuide.js")
 
-var client = new CommitGuide()
+var client = new CommitGuide({
+  admin: "397063436049186818"
+})
+
 client.on("ready",function(){
-    console.log(this)
-    client.admin = "397063436049186818"
-    
     client.listen("./CommitGuide/commands/")
 })
 

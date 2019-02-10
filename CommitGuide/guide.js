0a1b90390e86575cc6ea8f3b428219d6bde4a4ac
VonFriedricht <39649581+VonFriedricht@users.noreply.github.com>
2019-02-10 22:26:30 +0100

    Update guide.js

--- CommitGuide/guide.js
+++ CommitGuide/guide.js
@@ -1,6 +1,10 @@
 const CommitGuide = require("./CommitGuide.js")
 
-var client = new CommitGuide()
+var client = new CommitGuide({
+  top_left_day: process.env.top_left_day,
+  target_image: process.env.commit_image,
+  lyrics: process.env.lyrics.split("+")
+})
 
 client.on("ready",function(){
   client.listen("./CommitGuide/commands/")

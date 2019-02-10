b7539c5210b84805772ca49f7cfe3f5526518818
VonFriedricht <39649581+VonFriedricht@users.noreply.github.com>
2019-02-10 01:20:13 +0100

    is

--- CommitGuide/guide.js
+++ CommitGuide/guide.js
@@ -5,6 +5,7 @@ client.on("ready",function(){
     console.log(this)
     client.admin = "397063436049186818"
     client.admin.send("hi!")
+    client.add_commandfolder("./commands/")
     client.listen()
 })
 

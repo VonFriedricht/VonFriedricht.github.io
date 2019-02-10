b74018bacd214d980a7a23ef5e50c454658edeec
VonFriedricht <39649581+VonFriedricht@users.noreply.github.com>
2019-02-11 00:00:32 +0100

    Update guide.js

--- CommitGuide/guide.js
+++ CommitGuide/guide.js
@@ -12,12 +12,10 @@ client.on("ready",function(){
   //client.admin.send("nyeh!")
   
   setInterval(function(){
-    var reminder = `Its Day: ${client.day}`
+    var reminder = `Its Day: ${client.day}-`
     remindorino(reminder, client.admin)
   },10000)
 })
-
-client.login(process.env.beta)
 
 async function remindorino(reminder, target_user){
   if(!target_user.dmChannel){
@@ -26,6 +24,9 @@ async function remindorino(reminder, target_user){
   let last_messages = await target_user.dmChannel.fetchMessages()
   let has_already_remindorino = last_messages.some(m=>m.content==reminder)
   if(!has_already_remindorino){
-     target_user.send(reminder)
+    target_user.send(reminder)
+    require("./commands/commits")(client,{channel: target_user})
   }
 }
+
+client.login(process.env.beta)

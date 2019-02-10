1dad3203d369f58c7b06c44e5e3b507b1a8d0521
VonFriedricht <39649581+VonFriedricht@users.noreply.github.com>
2019-02-10 19:25:29 +0100

    Update commits.js

--- CommitGuide/commands/commits.js
+++ CommitGuide/commands/commits.js
@@ -14,16 +14,13 @@ module.exports = async (bot, message, args) => {
   // regular expression to find the data-count for the given date
   var target_reg = new RegExp(`data-count="(.*?)" data-date="${date}"`,"g")
   
-  var r = target_reg.exec(site)
-  message.channel.send(JSON.stringify(r))
-  //var todays_commits = await bot.get_todays_commits()
+  var r = target_reg.exec(sitecontent)
+  var todays_commits = await bot.get_todays_commits()
   
-  /*
   response.push(`todays commits: ${+r[1]}/${todays_commits}`)
   
   // check-value, to make sure everything is allright
   response.push(`(${r[0]})`)
   
   message.channel.send(response.join("\n"))
-  */
 }

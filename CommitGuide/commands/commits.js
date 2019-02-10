c93855db9b8b38828f71c655d9cefedef1fa2b5e
VonFriedricht <39649581+VonFriedricht@users.noreply.github.com>
2019-02-10 20:35:55 +0100

    Update commits.js

--- CommitGuide/commands/commits.js
+++ CommitGuide/commands/commits.js
@@ -4,23 +4,8 @@ module.exports = async (bot, message, args) => {
   var response = []
   var user = args || "VonFriedricht"
   
-  // date: format YYYY-MM-DD
-  var date = new Date().toISOString().split("T")[0]
+  var made_commits = await bot.get_made_commits()
+  var required_commits = await bot.get_required_commits()
   
-  // getting github page
-  var site = await axios.get(`https://github.com/${user}`)
-  var sitecontent = site.data
-  
-  // regular expression to find the data-count for the given date
-  var target_reg = new RegExp(`data-count="(.*?)" data-date="${date}"`,"g")
-  
-  var r = target_reg.exec(sitecontent)
-  var todays_commits = await bot.get_todays_commits()
-  
-  response.push(`todays commits: ${+r[1]}/${todays_commits}`)
-  
-  // check-value, to make sure everything is allright
-  response.push(`(${r[0]})`)
-  
-  message.channel.send(response.join("\n"))
+  message.channel.send(`${made_commits}/${required_commits}`)
 }

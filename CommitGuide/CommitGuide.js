886644b52a8e6604ea4560c19fcc4d396dc9ce41
VonFriedricht <39649581+VonFriedricht@users.noreply.github.com>
2019-02-10 18:29:18 +0100

    Update CommitGuide.js

--- CommitGuide/CommitGuide.js
+++ CommitGuide/CommitGuide.js
@@ -10,9 +10,9 @@ class CommitGuide extends GuideClient{
     this.lyrics = options.lyrics || false
     this.tile_sizes = options.tile_sizes || [0,1,5,10]
   }
-  /*async get_todays_commits(){
-    return 1+1
-  }*/
+  async get_todays_commits(){
+    return (await axios.get("http://aws.random.cat/meow")).data.file
+  }
 }
 
 module.exports = CommitGuide

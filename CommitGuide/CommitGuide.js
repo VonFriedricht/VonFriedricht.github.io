5b5dba44c0f1d7a473cf8eef21981bbe5920fd3e
VonFriedricht <39649581+VonFriedricht@users.noreply.github.com>
2019-02-10 20:32:45 +0100

    Update CommitGuide.js

--- CommitGuide/CommitGuide.js
+++ CommitGuide/CommitGuide.js
@@ -11,8 +11,25 @@ class CommitGuide extends GuideClient{
     this.lyrics = options.lyrics || false
     this.tile_sizes = options.tile_sizes || [0,1,5,10]
   }
-  async get_todays_commits(){
+  async get_required_commits(req_date){
+    var date = new Date(req_date)
     return (await axios.get("http://aws.random.cat/meow")).data.file
+  }
+  async get_made_commits(req_user="VonFriedricht", req_date=null){
+    // date: format YYYY-MM-DD
+    var date = new Date(req_date).toISOString().split("T")[0]
+
+    // getting github page
+    var site = await axios.get(`https://github.com/${req_user}`)
+    var sitecontent = site.data
+
+    // regular expression to find the data-count for the given date
+    var target_reg = new RegExp(`data-count="(.*?)" data-date="${date}"`,"g")
+
+    var reg_result = target_reg.exec(sitecontent)
+    var made_commits = reg_result[1]
+    
+    return made_commits
   }
 }
 

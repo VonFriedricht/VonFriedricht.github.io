04d3474fdbb75f7cfce02147916b4eff7479e3a1
VonFriedricht <39649581+VonFriedricht@users.noreply.github.com>
2019-02-10 19:07:48 +0100

    Update status.js

--- CommitGuide/commands/status.js
+++ CommitGuide/commands/status.js
@@ -1,6 +1,6 @@
-const axios = require("axios")
-
 module.exports = async (bot, message, args) => {
+  const axios = require("axios")
+  
   var response = []
   var user = args || "VonFriedricht"
   

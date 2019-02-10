1b987e70227cc8da2f88479e5d71245bcddde69b
VonFriedricht <39649581+VonFriedricht@users.noreply.github.com>
2019-02-10 11:22:01 +0100

    Update CommitGuide.js

--- CommitGuide/CommitGuide.js
+++ CommitGuide/CommitGuide.js
@@ -1,25 +1,21 @@
 const Discord = require("discord.js")
+const GuideClient = require("./GuideClient")
 
-class CommitGuide{
-  constructor(bot, options){
-    this.bot = bot
-    
+class CommitGuide extends GuideClient{
+  constructor(options){
     this.preview_tiles = options.preview_tiles || "─░▓█"
     this.top_left_day = options.top_left_day || "1970-01-01"
     this.target_image = options.target_image || "1111111122222413333344444444"
     this.lyrics = options.lyrics || false
     this.tile_sizes = options.tile_sizes || [0,1,5,10]
     this.admin = options.admin || -1
+    
+    this.admin.send("you shall be king")
   }
-  set bot(new_bot){
-    if(new_bot instanceof Discord.Client){
-       this._bot = new_bot
-    }
-    else{
-      throw "CommitGuide.bot must be Discord.Client"
-    }
+  set admin(new_admin){
+    this._admin = bot.users.find(u=>u.id==new_admin)
   }
-  get bot(){
-    retrun this._bot
+  get admin(){
+    return this._admin
   }
 }

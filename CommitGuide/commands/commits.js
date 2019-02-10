c3714b938a7401ff45c0817ec3e659b1cad8f569
VonFriedricht <39649581+VonFriedricht@users.noreply.github.com>
2019-02-10 20:40:28 +0100

    Update commits.js

--- CommitGuide/commands/commits.js
+++ CommitGuide/commands/commits.js
@@ -4,7 +4,7 @@ module.exports = async (bot, message, args) => {
   var response = []
   var user = args || "VonFriedricht"
   
-  var made_commits = await bot.get_made_commits()
+  var made_commits = await bot.get_made_commits("VonFriedricht")
   var required_commits = await bot.get_required_commits()
   
   message.channel.send(`${made_commits}/${required_commits}`)

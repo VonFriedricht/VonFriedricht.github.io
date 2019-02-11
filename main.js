a47c9a87b04ca9a266f81a7cfa80a1bc85ed1c6e
VonFriedricht <39649581+VonFriedricht@users.noreply.github.com>
2019-02-11 02:50:42 +0100

    right

--- main.js
+++ main.js
@@ -3,14 +3,14 @@ const bot = new Discord.Client()
 
 const CommitGuide = require("./CommitGuide.js")
 const commitguide = new CommitGuide(bot, {
-    top_left_day: process.env.top_left_day,
-    target_image: process.env.commit_image,
-    lyrics: process.env.lyrics.split("+"),
-    admin: process.env.admin
+  top_left_day: process.env.top_left_day,
+  target_image: process.env.commit_image,
+  lyrics: process.env.lyrics.split("+"),
+  admin: process.env.admin
 })
 
 bot.on("ready",async function(){
-    console.log("My Body is Ready!")
+  console.log("My Body is Ready!")
 })
 
 bot.login(process.env.destiny)

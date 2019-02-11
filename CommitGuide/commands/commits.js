682d75eda15b7fbcf8fd436a7d69936d7cedf620
VonFriedricht <39649581+VonFriedricht@users.noreply.github.com>
2019-02-11 18:46:45 +0100

    you

--- CommitGuide/commands/commits.js
+++ CommitGuide/commands/commits.js
@@ -7,6 +7,6 @@ module.exports = async (bot, message, args) => {
   var made_commits = await bot.get_made_commits("VonFriedricht")
   var required_commits = await bot.get_required_commits()
   
-  message.channel.send(`${made_commits}/${required_commits}`)
+  message.channel.send(`Commits: ${made_commits}/${required_commits}\n`)
   require("./next")(bot, message, required_commits-made_commits)
 }

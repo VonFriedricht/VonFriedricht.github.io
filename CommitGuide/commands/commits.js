0be5cc63657a5b319fdba1cf6e70b21b5bd04f6c
VonFriedricht <39649581+VonFriedricht@users.noreply.github.com>
2019-02-12 01:15:50 +0100

    Cause

--- CommitGuide/commands/commits.js
+++ CommitGuide/commands/commits.js
@@ -7,6 +7,8 @@ module.exports = async (bot, message, args) => {
   var made_commits = await bot.get_made_commits("VonFriedricht")
   var required_commits = await bot.get_required_commits()
   
-  message.channel.send(`Commits: ${made_commits}/${required_commits}\n`)
-  require("./next")(bot, message, required_commits-made_commits)
+  response.push(`Commits: ${made_commits}/${required_commits}\n`)
+  
+  var words = required_commits-made_commits
+  response.push(await bot.get_next_words(args))
 }

10a9304ce1b142e959c16f7f0de00cb015d51fb1
VonFriedricht <39649581+VonFriedricht@users.noreply.github.com>
2019-02-12 01:21:57 +0100

    going

--- CommitGuide/commands/commits.js
+++ CommitGuide/commands/commits.js
@@ -6,11 +6,11 @@ module.exports = async (bot, message, args) => {
   
   var made_commits = await bot.get_made_commits("VonFriedricht")
   var required_commits = await bot.get_required_commits()
-  response.push(`Commits: ${made_commits}/${required_commits}\n`)
+  response.push(`Commits: ${made_commits}/${required_commits}`)
   
   var wordcount = required_commits-made_commits
   var words = await bot.get_next_words(wordcount)
-  response.push(words.join("\n"))
+  response.push(words)
   
   message.channel.send(response.join("\n"))
 }

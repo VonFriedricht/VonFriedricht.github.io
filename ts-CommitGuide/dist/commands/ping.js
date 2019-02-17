66babf22517c56279e905df526037869558a1594
VonFriedricht <VonFriedricht@pm.me>
2019-02-17 10:10:03 +0100

    I'm

--- ts-CommitGuide/dist/commands/ping.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-module.exports = (bot, message, args) => {
-    message.reply("pong!");
-};
--- ts-CommitGuide/dist/commands/setactivity.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Command_1 = require("../Command");
-module.exports = new Command_1.Command("setActivity", (bot, message, args) => {
-    bot.user.setActivity(args);
-});

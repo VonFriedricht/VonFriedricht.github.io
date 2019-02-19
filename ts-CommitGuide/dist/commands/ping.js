11aeea49ededaa8d14d84988e59549ef56f02735
VonFriedricht <VonFriedricht@pm.me>
2019-02-19 15:02:59 +0100

    deleting old dist

--- ts-CommitGuide/dist/commands/ping.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Command_1 = require("../Command");
-let ping = new Command_1.Command("ping", function (bot, message, args) {
-    message.reply("pong!");
-});
-module.exports = ping;
--- ts-CommitGuide/dist/commands/setactivity.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Command_1 = require("../Command");
-let setActivity = new Command_1.Command("setActivity", function (bot, message, args) {
-    bot.user.setActivity(args);
-});
-module.exports = setActivity;

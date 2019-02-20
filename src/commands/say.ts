1824f0bfd1139fb4bf0061147adb8041f4aa0876
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-02-20 16:19:56 +0100

    I

---
+++ src/commands/say.ts
@@ -0,0 +1,12 @@
+import { Message } from "discord.js"
+
+function say(bot: CommitGuide, message: Message, args: string) {
+    message.channel.send(args)
+}
+
+function sayd(bot: CommitGuide, message: Message, args: string) {
+    message.channel.send(args)
+    message.delete()
+}
+
+module.exports = [say, sayd]

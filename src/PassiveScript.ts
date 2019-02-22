246dc00c8308ad28c1565f2cbd20b81e1db61250
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-02-22 22:12:01 +0100

    I

---
+++ src/PassiveScript.ts
@@ -0,0 +1,24 @@
+import { Client, Message } from "discord.js"
+
+export type PassiveFunc = (bot: Client) => void
+
+export class PassiveScript {
+    
+    func: PassiveFunc
+    interval: number
+    
+    constructor(func: PassiveFunc, interval: number) {
+        this.func = func
+        this.interval = interval
+    }
+    
+    exec(bot: Client) {
+        var target_client: Client = bot
+        var target_script: PassiveScript = this
+
+        setInterval(function() {
+            target_script.func(target_client)
+        }, this.interval)
+    }
+    
+}

c4b250de9d7b418428d0df3176348d942ec76e3b
VonFriedricht <VonFriedricht@pm.me>
2019-02-23 00:08:50 +0100

    every

--- src/passive/daily_commitcheck.ts
+++ src/passive/daily_commitcheck.ts
@@ -1,10 +1,11 @@
 import { PassiveScript, PassiveFunc } from "../PassiveScript"
 import { CommitGuide } from "../CommitGuide"
+import { hour } from "../time";
 
 var script: PassiveFunc = function(bot: CommitGuide) {
     console.log(123)
 }
 
-var daily_commitcheck = new PassiveScript(script, 6000)
+var daily_commitcheck = new PassiveScript(script, 1*hour)
 
 module.exports = daily_commitcheck
--- src/time.ts
+++ src/time.ts
@@ -1,7 +1,11 @@
 export let ms = 1
 export let s = 1000*ms
+
 export let min = 60*s
+
 export let h = 60*min
+export let hour = h
+
 export let day = 24*h
 export let week = 7*day
 export let month = 30*day

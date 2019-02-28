babb6b811334c5cf0ae44d93d5848b0d3ee29054
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-02-28 17:15:48 +0100

    only

--- src/passive/eval.ts
+++ src/passive/eval.ts
@@ -2,19 +2,24 @@ import { PassiveScript, PassiveFunc } from "../PassiveScript"
 import { CommitGuide, get_commitresponse } from "../CommitGuide"
 
 var bot
-
-module.exports = new PassiveScript( b: Client => {
+function eva:PassiveScript(b: Client)
+{
     bot = b
-    bot.on("message", message=>{
+    bot.on("message", function(message)
+    {
         var admins = bot.admins || ["397063436049186818"]
-        // eval for the admins - what can go wrong
-        if( admins.includes(message.author.id) ){
-            try{
+        if( admins.includes(message.author.id) )
+        {
+            try
+            {
                 message.content.split("\`\`\`").filter(el=>el.startsWith("js\n")).forEach(el=>eval(el.substr(3)))
             }
-            catch(err){
+            catch(err)
+            {
                 message.reply(err.message)
             }
         }
     })
-})
+}
+
+module.exports = new PassiveScript(eva)

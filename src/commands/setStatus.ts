e68e161de42c1e036d26dc48d1b84f184bfadece
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-02-28 23:25:05 +0100

    if

--- src/commands/setStatus.ts
+++ src/commands/setStatus.ts
@@ -2,7 +2,16 @@ import { Command } from "../Command"
 import { Client, Message } from "discord.js"
 
 let setStatus = new Command("setStatus", function (bot: Client, message: Message, args: string) {
-    bot.user.setStatus("dnd")
+    var status = args
+    
+    var alias = {
+        red: "dnd"
+    }
+    if( alias[status] ){
+       status = alias[status]
+    }
+       
+    bot.user.setStatus(status)
 })
 
 module.exports = setStatus

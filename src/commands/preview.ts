2677f240673387f565850a3317bafa6754db6d5f
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-03-01 20:11:07 +0100

    my

--- src/commands/preview.ts
+++ src/commands/preview.ts
@@ -1,3 +1,7 @@
 module.exports = async (bot, message, args) => {
-    message.channel.send(bot.target_image)
+    let weekdays = new Array(7).map(e=>"")
+    bot.target_image((v,i)=>{
+        weekdays[i%7] += ""+v
+    })
+    weekdays.forEach(v=>message.channel.send(v))
 }

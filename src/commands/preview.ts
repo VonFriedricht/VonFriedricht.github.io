96367ee0b57b305309f8c651757c47afb3852180
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-03-01 20:24:26 +0100

    will

--- src/commands/preview.ts
+++ src/commands/preview.ts
@@ -1,7 +1,8 @@
 module.exports = async (bot, message, args) => {
     let weekdays = ["","","","","","",""]
     bot.target_image.forEach((v,i)=>{
-        weekdays[i%7] += String(v)
+        let e = bot.preview_tiles[v-1]
+        weekdays[i%7] += e+e
     })
-    message.channel.send('`'+weekdays.join("\n")+'`')
+    message.channel.send(weekdays.join("\n"))
 }

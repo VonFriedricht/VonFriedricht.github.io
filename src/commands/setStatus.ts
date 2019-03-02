e720f72d634559936b53e0014e27bb998505d857
VonFriedricht <VonFriedricht@pm.me>
2019-03-02 23:37:14 +0100

    come

--- src/commands/setStatus.ts
+++ src/commands/setStatus.ts
@@ -3,21 +3,20 @@ import { Client, Message, PresenceStatus } from "discord.js"
 
 type AdvancedPresenceStatus = PresenceStatus | "red" | "green" | "yellow" | "grey";
 let Presences:PresenceStatus[] = ["online", "dnd", "idle", "invisible"];
-
-interface palias {
-    red: PresenceStatus
-}
+let PresencesString:string[] = ["online", "dnd", "idle", "invisible"];
 
 let setStatus = new Command("setStatus", function (bot: Client, message: Message, args: string) {
 
     if( isFinite(Number(args)) ){
         var index = Number(status)
         bot.user.setStatus(Presences[index])
-    }/*
+    }
     else{
-        bot.user.setStatus(args)
-    }*/
-    
+        let p_index = PresencesString.indexOf(args)
+        if( p_index != -1 ){
+            bot.user.setStatus(Presences[p_index])
+        }
+    }
        
 })
 

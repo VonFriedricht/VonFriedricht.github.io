24a2e43cc8c76b243f0819089d95d69b268470d1
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-03-08 23:46:08 +0100

    Come

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -3,6 +3,6 @@ import { Client, Message } from "discord.js"
 
 let ping = new Command ("ping", function (bot: Client, message: Message, args: string) {
     message.reply("pong!")
-})
+}, {dm: false})
 
 module.exports = ping 

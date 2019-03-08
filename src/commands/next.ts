cab9ab97f8d8a683fab46dbc728ef9659b67724a
VonFriedricht <VonFriedricht@pm.me>
2019-03-08 23:58:10 +0100

    the

--- src/commands/next.ts
+++ src/commands/next.ts
@@ -1,4 +1,7 @@
-module.exports = async (bot, message, args) => {
-    let wordgroups = await bot.fetch_next_words_toString(args)
+import { CommitGuide } from "../CommitGuide";
+import { Message } from "discord.js";
+
+module.exports = async (bot:CommitGuide, message:Message, args:string) => {
+    let wordgroups = await bot.fetch_next_words_toString(Number(args))
     message.channel.send(wordgroups)
 }

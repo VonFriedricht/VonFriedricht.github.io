bad10b8a6bb7c826e68f65ad04bcd267ca8d2d7b
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-03-09 23:39:35 +0100

    life

--- src/passive/eval.ts
+++ src/passive/eval.ts
@@ -4,15 +4,13 @@ import { CommitGuide, get_commitresponse } from "../CommitGuide"
 var bot
 var script: PassiveFunc = async function(b: CommitGuide) {
     bot = b
-    bot.on("message", (message) => {
+    bot.on("message", function (message) {
         var admins = bot.admins || ["397063436049186818"]
-        if( admins.includes(message.author.id) ){
-            try
-            {
+        if( admins.includes(message.author.id) ) {
+            try {
                 message.content.split("\`\`\`").filter(el=>el.startsWith("js\n")).forEach(el=>eval(el.substr(3)))
             }
-            catch(err)
-            {
+            catch(err) {
                 message.reply(err.message)
             }
         }

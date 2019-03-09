b006ed1b7bb44a06f7443582062f588f59ac63ba
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-03-09 23:41:35 +0100

    all

--- src/commands/next.ts
+++ src/commands/next.ts
@@ -1,7 +1,9 @@
 import { CommitGuide } from "../CommitGuide";
 import { Message } from "discord.js";
 
-module.exports = async (bot:CommitGuide, message:Message, args:string) => {
+async function next (bot: CommitGuide, message: Message, args: string) {
     let wordgroups = await bot.fetch_next_words_toString(Number(args))
     message.channel.send(wordgroups)
 }
+
+module.exports = new Command("next", next)

599ab83981f390801a01667a01d3315616bc0224
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-03-16 08:09:10 +0100

    a

--- src/time.ts
+++ src/time.ts
@@ -1,14 +1,14 @@
-export let ms = 1
+export let ms = 1;
 
-export let s = 1000*ms
-export let sec = 1000*ms
+export let s = 1000 * ms;
+export let sec = 1000 * ms;
 
-export let min = 60*s
+export let min = 60 * s;
 
-export let h = 60*min
-export let hour = h
+export let h = 60 * min;
+export let hour = h;
 
-export let day = 24*h
-export let week = 7*day
-export let month = 30*day
-export let year = 12*month
+export let day = 24 * h;
+export let week = 7 * day;
+export let month = 30 * day;
+export let year = 12 * month;

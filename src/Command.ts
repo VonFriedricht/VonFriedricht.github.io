9663689d2ae938685f69d0d16a96da1b08ba857f
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-03-16 08:08:04 +0100

    still

--- src/Command.ts
+++ src/Command.ts
@@ -1,61 +1,64 @@
-import { Client, Message, GuildMember, Channel } from "discord.js"
+import { Client, Message, GuildMember, Channel } from "discord.js";
 
 // export type CommandName = /^[A-Za-z0-9]+$/ ♥ please make it happen
-export type CustomCommand = (bot: Client, message: Message, args: string) => void
+export type CustomCommand = (
+  bot: Client,
+  message: Message,
+  args: string
+) => void;
 
-interface CommandOptions{
-    roles?: string[];
-    dm?: boolean
+interface CommandOptions {
+  roles?: string[];
+  dm?: boolean;
 }
 
 export class Command {
+  _name: string; // CommandName
+  func: CustomCommand;
+  allowed_roles: string[]; //roles[] TBD
+  allowed_dm: boolean;
 
-    _name: string // CommandName
-    func: CustomCommand
-    allowed_roles: string[] //roles[] TBD
-    allowed_dm: boolean
+  constructor(name: string, func: CustomCommand, options: CommandOptions = {}) {
+    this.name = name;
+    this.func = func;
+    this.allowed_roles = options.roles || ["*"];
+    this.allowed_dm = options.dm || true;
+  }
 
-    constructor(name: string, func: CustomCommand, options: CommandOptions = {}) {
-        this.name = name
-        this.func = func
-        this.allowed_roles = options.roles || ["*"]
-        this.allowed_dm = options.dm || true
+  set name(name: string) {
+    this._name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  isPermitted(member: GuildMember, channel: Channel) {
+    if (channel.type == "text") {
+      if (this.allowed_roles.length > 0) {
+        return true;
+      }
+      if (this.allowed_roles.includes("*")) {
+        return true;
+      }
+      if (member.roles.some(r => this.allowed_roles.includes(r.name))) {
+        return true;
+      }
     }
-
-    set name(name: string) {
-        this._name = name
+    if (channel.type == "dm" && this.allowed_dm) {
+      return true;
     }
+    return false;
+  }
 
-    get name() {
-        return this._name
+  execute(bot: Client, message: Message, args: string) {
+    if (this.isPermitted(message.member, message.channel)) {
+      console.log(`Executing "${this.name}" with args: "${args}"`);
+      this.func(bot, message, args);
+    } else {
+      console.log(
+        `${message.author.username} is not permitted to do ${this.name}`
+      );
     }
-
-    isPermitted(member: GuildMember, channel: Channel){
-        if( channel.type == "text" ){
-            if( this.allowed_roles.length > 0 ){
-               return true
-            }
-            if( this.allowed_roles.includes("*") ){
-                return true
-            }
-            if( member.roles.some(r=>this.allowed_roles.includes(r.name)) ){
-                return true
-            }
-        }
-        if( channel.type == "dm" && this.allowed_dm ){
-            return true
-        }
-        return false
-    }
-    
-    execute(bot: Client, message: Message, args: string) {
-        if( this.isPermitted(message.member, message.channel) ){
-            console.log(`Executing "${this.name}" with args: "${args}"`)
-            this.func(bot, message, args)
-        }
-        else{
-            console.log(`${message.author.username} is not permitted to do ${this.name}`)
-        }
-    }
-
+  }
 }

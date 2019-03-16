6ddb0b403f3e192e8f8bb1637053e6ab412a73ed
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-03-16 08:10:23 +0100

    breeze

--- src/passive/daily_commitcheck.ts
+++ src/passive/daily_commitcheck.ts
@@ -1,25 +1,24 @@
-import { PassiveScript, PassiveFunc } from "../PassiveScript"
-import { CommitGuide, get_commitresponse } from "../CommitGuide"
+import { PassiveScript, PassiveFunc } from "../PassiveScript";
+import { CommitGuide, get_commitresponse } from "../CommitGuide";
 import { hour, min, sec } from "../time";
 
 var script: PassiveFunc = async function(bot: CommitGuide) {
-    let vnft = bot.users.find(u=>u.id=="397063436049186818")
-    let flag = `Current Day: ${bot.day}`
+  let vnft = bot.users.find(u => u.id == "397063436049186818");
+  let flag = `Current Day: ${bot.day}`;
 
-    if( !vnft.dmChannel ) {
-        vnft.send("🤔")
-        return false
+  if (!vnft.dmChannel) {
+    vnft.send("🤔");
+    return false;
+  } else {
+    let messages = await vnft.dmChannel.fetchMessages();
+    let has_send = messages.some(m => m.content == flag);
+    if (!has_send) {
+      vnft.send(flag);
+      vnft.send(await get_commitresponse(bot));
     }
-    else {
-        let messages = await vnft.dmChannel.fetchMessages()
-        let has_send = messages.some(m=>m.content==flag)
-        if(!has_send) {
-            vnft.send(flag)
-            vnft.send(await get_commitresponse(bot))
-        }
-    }
-}
+  }
+};
 
-var daily_commitcheck = new PassiveScript(script, hour)
+var daily_commitcheck = new PassiveScript(script, hour);
 
-module.exports = daily_commitcheck
+module.exports = daily_commitcheck;

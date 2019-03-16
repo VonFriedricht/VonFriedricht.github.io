1d2472109b5506936692f3b1e5cc6fcc91cef9d2
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-03-16 09:06:53 +0100

    hard

--- src/CommitGuide.ts
+++ src/CommitGuide.ts
@@ -34,6 +34,31 @@ export class CommitGuide extends CommandHandler {
     return day_int;
   }
 
+  async fetch_made_commits(username: string): Promise<number> {
+    // today: format YYYY-MM-DD
+    let today_ISO = new Date().toISOString().split("T")[0];
+
+    // getting github page
+    let site = await axios.get(`https://github.com/` + username);
+    let sitecontent: string = site.data;
+
+    // regular expression to find the data-count for the given date
+    let target_reg = new RegExp(
+      `data-count="(.*?)" data-date="${today_ISO}"`,
+      "g"
+    );
+    let reg_result = target_reg.exec(sitecontent);
+    let made_commits = reg_result[1];
+
+    return Number(made_commits);
+  }
+
+  get required_commits(): number {
+    let daytile = this.target_image[this.day];
+    let daysize = this.tile_sizes[daytile - 1];
+    return daysize;
+  }
+
   async fetch_next_words(count: number): Promise<string[][]> {
     let words: string[] = this.lyrics;
     let header: string[] = await this.fetch_last_commits(25);

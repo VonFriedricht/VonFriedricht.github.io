795e7f2ba05bab5f6608292b7e6c97025db5c7a8
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-03-28 17:11:33 +0100

    Signs

--- src/commands/commits.ts
+++ src/commands/commits.ts
@@ -1,9 +1,13 @@
-import { Message } from "discord.js"
+import { Message } from "discord.js";
 import { CommitGuide, get_commitresponse } from "../CommitGuide";
 
-export async function commits(bot: CommitGuide, message: Message, args: string) {
-    message.channel.send(`Current Day: ${bot.day}`)
-    message.channel.send( await get_commitresponse(bot, args) )
+export async function commits(
+  bot: CommitGuide,
+  message: Message,
+  args: string
+) {
+  message.channel.send(`Current Day: ${bot.day}`);
+  message.channel.send(await get_commitresponse(bot, args));
 }
 
-module.exports = commits
+module.exports = commits;

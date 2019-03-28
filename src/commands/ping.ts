a26b5a8b361962e1ad02dd7a154b04a1a3ebf344
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-03-28 17:10:40 +0100

    pray

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,8 +1,8 @@
-import { Command } from "../Command"
-import { Client, Message } from "discord.js"
+import { Command } from "../Command";
+import { Client, Message } from "discord.js";
 
 function ping(bot: Client, message: Message, args: string) {
-    message.reply("pong!")
+  message.reply("pong!");
 }
 
-module.exports = new Command ("ping", ping, {dm: false})
+module.exports = new Command("ping", ping, { dm: false });

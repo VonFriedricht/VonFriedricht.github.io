c8346acd520c17af1bd6e89af99ac4353b36f911
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-03-28 17:12:13 +0100

    warning

--- src/commands/preview.ts
+++ src/commands/preview.ts
@@ -1,14 +1,14 @@
-import { Message } from "discord.js"
+import { Message } from "discord.js";
 import { CommitGuide } from "../CommitGuide";
 import { Command } from "../Command";
 
 async function preview(bot: CommitGuide, message: Message, args: string) {
-    let weekdays = ["","","","","","",""]
-    bot.target_image.forEach((v,i)=>{
-        let e = bot.preview_tiles[v-1]
-        weekdays[i%7] += e+e
-    })
-    message.channel.send(weekdays.join("\n"))
+  let weekdays = ["", "", "", "", "", "", ""];
+  bot.target_image.forEach((v, i) => {
+    let e = bot.preview_tiles[v - 1];
+    weekdays[i % 7] += e + e;
+  });
+  message.channel.send(weekdays.join("\n"));
 }
 
-module.exports = new Command("preview", preview)
+module.exports = new Command("preview", preview);

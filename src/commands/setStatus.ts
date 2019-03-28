98748d7ec1b922b3b3d26e09d9e16223fbec5b0b
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-03-28 17:24:38 +0100

    life

--- src/commands/setStatus.ts
+++ src/commands/setStatus.ts
@@ -1,23 +1,29 @@
-import { Command } from "../Command"
-import { Client, Message, PresenceStatus } from "discord.js"
+import { Command } from "../Command";
+import { Client, Message, PresenceStatus } from "discord.js";
 
-type AdvancedPresenceStatus = PresenceStatus | "red" | "green" | "yellow" | "grey";
-let Presences:PresenceStatus[] = ["online", "dnd", "idle", "invisible"];
-let PresencesString:string[] = ["online", "dnd", "idle", "invisible"];
+type AdvancedPresenceStatus =
+  | PresenceStatus
+  | "red"
+  | "green"
+  | "yellow"
+  | "grey";
+let Presences: PresenceStatus[] = ["online", "dnd", "idle", "invisible"];
+let PresencesString: string[] = ["online", "dnd", "idle", "invisible"];
 
-let setStatus = new Command("setStatus", function (bot: Client, message: Message, args: string) {
-
-    if( isFinite(Number(args)) ){
-        var index = Number(status)
-        bot.user.setStatus(Presences[index])
+let setStatus = new Command("setStatus", function(
+  bot: Client,
+  message: Message,
+  args: string
+) {
+  if (isFinite(Number(args))) {
+    var index = Number(status);
+    bot.user.setStatus(Presences[index]);
+  } else {
+    let p_index = PresencesString.indexOf(args);
+    if (p_index != -1) {
+      bot.user.setStatus(Presences[p_index]);
     }
-    else{
-        let p_index = PresencesString.indexOf(args)
-        if( p_index != -1 ){
-            bot.user.setStatus(Presences[p_index])
-        }
-    }
-       
-})
+  }
+});
 
-module.exports = setStatus
+module.exports = setStatus;

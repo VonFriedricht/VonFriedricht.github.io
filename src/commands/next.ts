89b8559eee412f41c3c0bac79b5748ad3b6d39dc
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-03-28 17:11:51 +0100

    of

--- src/commands/next.ts
+++ src/commands/next.ts
@@ -2,9 +2,9 @@ import { CommitGuide } from "../CommitGuide";
 import { Message } from "discord.js";
 import { Command } from "../Command";
 
-async function next (bot: CommitGuide, message: Message, args: string) {
-    let wordgroups = await bot.fetch_next_words_toString(Number(args))
-    message.channel.send(wordgroups)
+async function next(bot: CommitGuide, message: Message, args: string) {
+  let wordgroups = await bot.fetch_next_words_toString(Number(args));
+  message.channel.send(wordgroups);
 }
 
-module.exports = new Command("next", next)
+module.exports = new Command("next", next);

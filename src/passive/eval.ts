641295545bf93944ec4e5546d0633a5261cceb7f
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-03-29 08:24:35 +0100

    become

--- src/passive/eval.ts
+++ src/passive/eval.ts
@@ -1,22 +1,24 @@
-import { PassiveScript, PassiveFunc } from "../PassiveScript"
-import { CommitGuide, get_commitresponse } from "../CommitGuide"
+import { PassiveScript, PassiveFunc } from "../PassiveScript";
+import { CommitGuide, get_commitresponse } from "../CommitGuide";
 
-var bot
+var bot;
 var script: PassiveFunc = async function(b: CommitGuide) {
-    bot = b
-    bot.on("message", function (message) {
-        var admins = bot.admins || ["397063436049186818"]
-        if( admins.includes(message.author.id) ) {
-            try {
-                message.content.split("\`\`\`").filter(el=>el.startsWith("js\n")).forEach(el=>eval(el.substr(3)))
-            }
-            catch(err) {
-                message.reply(err.message)
-            }
-        }
-    })
-}
+  bot = b;
+  bot.on("message", function(message) {
+    var admins = bot.admins || ["397063436049186818"];
+    if (admins.includes(message.author.id)) {
+      try {
+        message.content
+          .split("```")
+          .filter(el => el.startsWith("js\n"))
+          .forEach(el => eval(el.substr(3)));
+      } catch (err) {
+        message.reply(err.message);
+      }
+    }
+  });
+};
 
-var passive_eva = new PassiveScript(script)
+var passive_eva = new PassiveScript(script);
 
-module.exports = passive_eva
+module.exports = passive_eva;

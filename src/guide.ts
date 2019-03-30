01d95bf850b17bebf8f4ad0904729397e70d5b64
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-03-30 18:50:30 +0100

    my

--- src/guide.ts
+++ src/guide.ts
@@ -1,15 +1,15 @@
-import { CommitGuide } from "./CommitGuide"
+import { CommitGuide } from "./CommitGuide";
 
 var bot = new CommitGuide({
-    top_left_day: process.env.top_left_day,
-    target_image: process.env.commit_image.split("").map(e=>Number(e)),
-    lyrics: process.env.lyrics.split("+")
-})
+  top_left_day: process.env.top_left_day,
+  target_image: process.env.commit_image.split("").map(e => Number(e)),
+  lyrics: process.env.lyrics.split("+")
+});
 
-bot.read_commanddir(__dirname+"/commands")
-bot.exec_passivedir(__dirname+"/passive")
+bot.read_commanddir(__dirname + "/commands");
+bot.exec_passivedir(__dirname + "/passive");
 
-bot.login(process.env.destiny).then(()=>{
-    let vnft = bot.users.find(u=>u.id=="397063436049186818")
-    bot.listen_user(vnft)
-})
+bot.login(process.env.destiny).then(() => {
+  let vnft = bot.users.find(u => u.id == "397063436049186818");
+  bot.listen_user(vnft);
+});

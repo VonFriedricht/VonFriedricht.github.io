8fb2381dd617f1eac3821703454e2d6defd9af18
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-04-03 07:29:48 +0200

    enemy

---
+++ src/commands/setActivity.ts
@@ -0,0 +1,8 @@
+import { Command } from "../Command";
+import { Client, Message } from "discord.js";
+
+function setActivity(bot: Client, message: Message, args: string){
+  bot.user.setActivity(args);
+}
+
+module.exports = new Command("setActivity", setActivity);

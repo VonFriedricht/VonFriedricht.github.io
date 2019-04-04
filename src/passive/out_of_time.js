5d9caa0db8794c59b91d9f4e0a1b0e4902f4feac
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-04-04 22:16:01 +0200

    I'm

--- src/passive/out_of_time.js
+++ src/passive/out_of_time.js
@@ -1,1 +1,17 @@
+import { PassiveScript, PassiveFunc } from "../PassiveScript";
+import { hour } from "../time";
 
+async function main(bot: CommitGuide): PassiveFunc {
+  let vnft = bot.users.find(u => u.id == "397063436049186818"); // lol, make it an ENV
+  
+  if (!vnft.dmChannel) {
+    vnft.send("🤔");
+    return false;
+  } else {
+    let made_commits = await bot.fetch_made_commits(user)
+    vnft.send(made_commits)
+  }
+}
+
+var out_of_time = new PassiveScript(script, hour);
+module.exports = out_of_time;

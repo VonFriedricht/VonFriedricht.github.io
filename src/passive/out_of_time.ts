2ad2159f807b9b5ac06191e22e04c7b1176a9ea5
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-04-12 20:06:59 +0200

    words

--- src/passive/out_of_time.ts
+++ src/passive/out_of_time.ts
@@ -5,24 +5,24 @@ import { CommitGuide } from "../CommitGuide";
 async function script(bot: CommitGuide) {
   let vnft = bot.users.find(u => u.id == "397063436049186818"); // lol, make it an ENV
   let user = "VonFriedricht"; // lol this too
-  
+
   if (!vnft.dmChannel) {
     vnft.send("🤔");
     return false;
   } else {
-    let made_commits: number = await bot.fetch_made_commits(user)
-    let required_commits: number = bot.required_commits
-    let done = made_commits >= required_commits
-    if( !done ){
-      let time_short = new Date().getHours() > 15
-      if( time_short ){
+    let made_commits: number = await bot.fetch_made_commits(user);
+    let required_commits: number = bot.required_commits;
+    let done = made_commits >= required_commits;
+    if (!done) {
+      let time_short = new Date().getHours() > 15;
+      if (time_short) {
         // remind
-        let reminder = `${made_commits} by ${required_commits} done`
-        vnft.send(reminder)
+        let reminder = `${made_commits} by ${required_commits} done`;
+        vnft.send(reminder);
       }
     }
   }
 }
 
-var out_of_time = new PassiveScript(script, 0.5*hour+minute);
+var out_of_time = new PassiveScript(script, 0.5 * hour + minute);
 module.exports = out_of_time;

1484eba011ced9ee8810f5464cd302f81210e34c
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-04-23 18:32:39 +0200

    to

--- src/re_guide/classes/Guide.ts
+++ src/re_guide/classes/Guide.ts
@@ -1,7 +1,20 @@
 import { CommandHandler } from "./CommandHandler";
 
 export class Guide extends CommandHandler {
+  target_image: number[];
+  top_left_day: Date;
+  preview_tiles: string[];
+
   constructor() {
     super();
+    this.target_image = [1, 2, 3, 4];
+    this.top_left_day = new Date();
+    this.preview_tiles = ["A", "B", "C", "D", "E"];
+  }
+
+  get day(): number {
+    var day = (new Date().getTime() - new Date(this.top_left_day).getTime()) / 86400000;
+    var day_int = Math.floor(day);
+    return day_int;
   }
 }
--- src/re_guide/main.ts
+++ src/re_guide/main.ts
@@ -6,4 +6,8 @@ let guide = new Guide();
 guide.loadCommands(path.join(__dirname, "commands"));
 guide.loadScripts(path.join(__dirname, "scripts"));
 
+guide.preview_tiles = ["─", "░", "▓", "█"];
+guide.top_left_day = new Date(process.env.top_left_day);
+guide.target_image = process.env.commit_image.split("").map(e => Number(e));
+
 guide.login(process.env.discord_token);

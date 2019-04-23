ae7be479e398727fe9ad20e42a6306068c7a7b02
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-04-23 18:41:56 +0200

    life...

--- src/re_guide/scripts/test.ts
+++
@@ -1,8 +0,0 @@
-import { Client } from "discord.js";
-import { Script } from "../classes/Script";
-
-function test(bot: Client) {
-  console.log(bot.users.find(u => u.username == "VonFriedricht").id);
-}
-
-module.exports = new Script(test, 1000 * 60);

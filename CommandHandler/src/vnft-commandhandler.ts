63921bbc725939abe2e28b262a54440ae36a783d
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-04-26 10:22:22 +0200

    carrying

--- CommandHandler/src/vnft-commandhandler.ts
+++ CommandHandler/src/vnft-commandhandler.ts
@@ -1,8 +1,6 @@
 import { Client, Message } from "discord.js";
 import * as fs from "fs";
-import { Command } from "./Command";
 import { fetchJS } from "vnft-tools";
-import { Script } from "./Script";
 
 export class CommandHandler extends Client {
   commands: Command[];
@@ -86,6 +84,65 @@ export class CommandHandler extends Client {
           }
         }
       }
+    }
+  }
+}
+
+type CommandFunction = (bot: Client, message: Message, args: string) => any;
+
+export class Command {
+  _name: string;
+  _funct: CommandFunction;
+  type: string;
+
+  constructor(name?: string, funct?: CommandFunction) {
+    this.name = name;
+    this.funct = funct;
+    this.type = "Command";
+  }
+
+  set name(name: string) {
+    this._name = name;
+  }
+  get name(): string {
+    return this._name;
+  }
+
+  set funct(funct: CommandFunction) {
+    this._funct = funct;
+  }
+  get funct(): CommandFunction {
+    return this._funct;
+  }
+
+  execute(bot: Client, message: Message) {
+    console.log(`Executing: `, this);
+    let params = message.content.match(/.*?\s(.*$)/);
+    let args: string = params ? params[1] : "";
+    this.funct(bot, message, args);
+  }
+}
+
+export type ScriptFunction = (bot: Client) => any;
+
+export class Script {
+  funct: ScriptFunction;
+  intervalTime: number;
+  interval: NodeJS.Timeout;
+  type: String;
+  triggered: boolean;
+
+  constructor(funct: ScriptFunction, interval: number) {
+    this.funct = funct;
+    this.intervalTime = interval;
+    this.type = "Script";
+    this.triggered = false;
+  }
+
+  trigger(bot: Client) {
+    if (this.triggered == false) {
+      this.interval = setInterval(this.funct, this.intervalTime, bot);
+      this.triggered = true;
     }
   }
 }

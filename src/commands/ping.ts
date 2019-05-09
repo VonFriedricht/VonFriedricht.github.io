d05bb8022ed2684363e255254d73de2dd3412d07
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-05-09 22:45:54 +0200

    going

--- src/commands/ping.ts
+++
@@ -1,8 +0,0 @@
-import { Command } from "../Command";
-import { Client, Message } from "discord.js";
-
-function ping(bot: Client, message: Message, args: string): void {
-  message.reply("pong!");
-}
-
-module.exports = new Command("ping", ping, { dm: false });
--- src/commands/setActivity.ts
+++
@@ -1,8 +0,0 @@
-import { Command } from "../Command";
-import { Client, Message } from "discord.js";
-
-function setActivity(bot: Client, message: Message, args: string): void {
-  bot.user.setActivity(args);
-}
-
-module.exports = new Command("setActivity", setActivity);

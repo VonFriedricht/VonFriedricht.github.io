04bdd5c93bc5a059e6e58d049a41007a765bc723
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-05-10 21:51:36 +0200

    if

--- src/re_guide/scripts/test.ts
+++ src/re_guide/scripts/test.ts
@@ -6,7 +6,7 @@ const test = new Script();
 test.funct = (bot: Client) => {
     let vnft = bot.users.find(u=>u.id=="397063436049186818")
     if( !vnft.dmChannel ){
-        vnft.send(" ").catch()
+        vnft.send(" ").catch(e=>console.log(`reeee: ${e}`))
     }
     else{
         console.log(vnft.dmChannel)

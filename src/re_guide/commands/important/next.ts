b90ac06bd21cd8c5a98251247961e533a32f13de
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-05-11 22:01:32 +0200

    I

---
+++ src/re_guide/commands/important/next.ts
@@ -0,0 +1,19 @@
+import { Command } from "vnft-commandhandler/lib/src";
+import { Guide } from "re_guide/classes/Guide";
+import { Message } from "discord.js";
+
+const next = new Command();
+next.name = "next";
+
+next.funct = async (bot: Guide, message: Message, args: string = "5"): Promise<void> => {
+  // Message:  commits
+  let wordcount = Number(args);
+  let wordgroups = await bot.nextWords(wordcount);
+  let response = [];
+  for (let wordgroup of wordgroups) {
+    response.push(`\`${wordgroup.join("\n")}\``);
+  }
+  message.channel.send(response.join("\n"));
+};
+
+module.exports = next;

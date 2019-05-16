43c5235fd91a47a67aa6729c7478505d8afb7c50
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-05-16 19:51:18 +0200

    truth

--- src/commands/imgtest.ts
+++ src/commands/imgtest.ts
@@ -2,14 +2,32 @@ import { Command } from "vnft-commandhandler";
 import { Attachment } from "discord.js";
 
 const Canvas = require("canvas");
+const colors = [
+  "#ebedf0",
+  "#c6e48b",
+  "#7bc96f",
+  "#196127"
+];
 
 const test = new Command();
 test.name = "test";
 test.funct = async (bot, message, args) => {
   const canvas = Canvas.createCanvas(250, 250);
   const ctx = canvas.getContext("2d");
+  
+  const pixels = [0,1,2,3];
 
-  ctx.fillRect(50, 50, 150, 150);
+  let x = 0;
+  let y = 0;
+  for( let pixel of pixels ){
+    ctx.fillStyle = colors[pixel]
+    ctx.fillRect(x*11, y*11, 10, 10);
+    x++
+    if(x==7){
+      y++
+      x=0
+    }
+  }
 
   const attachment = new Attachment(canvas.toBuffer(), `yee.png`);
   message.channel.send(args, attachment);

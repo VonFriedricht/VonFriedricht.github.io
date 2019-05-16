7fa5e408859aefb7362c381f723dfaf46b50c6e3
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-05-16 20:30:04 +0200

    is

--- src/commands/imgtest.ts
+++ src/commands/imgtest.ts
@@ -1,8 +1,10 @@
 import { Command } from "vnft-commandhandler";
 import { Attachment } from "discord.js";
+import { Guide } from "classes/Guide";
 
 const Canvas = require("canvas");
 const colors = [
+  null, 
   "#ebedf0",
   "#c6e48b",
   "#7bc96f",
@@ -11,21 +13,21 @@ const colors = [
 
 const test = new Command();
 test.name = "test";
-test.funct = async (bot, message, args) => {
-  const canvas = Canvas.createCanvas(250, 250);
+test.funct = async (bot:Guide, message, args) => {
+  const pixels = bot.target_image;
+
+  const canvas = Canvas.createCanvas(Math.ceil(pixels.length/7)*11, 11*7);
   const ctx = canvas.getContext("2d");
-  
-  const pixels = [0,1,2,3];
 
   let x = 0;
   let y = 0;
   for( let pixel of pixels ){
     ctx.fillStyle = colors[pixel]
     ctx.fillRect(x*11, y*11, 10, 10);
-    x++
-    if(x==7){
-      y++
-      x=0
+    y++
+    if(y==7){
+      x++
+      y=0
     }
   }
 

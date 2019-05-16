a2649f48f3bfdfa31a0a181af9e3db74c1aa2003
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-05-16 20:30:35 +0200

    that

--- src/commands/imgtest.ts
+++ src/commands/imgtest.ts
@@ -3,31 +3,25 @@ import { Attachment } from "discord.js";
 import { Guide } from "classes/Guide";
 
 const Canvas = require("canvas");
-const colors = [
-  null, 
-  "#ebedf0",
-  "#c6e48b",
-  "#7bc96f",
-  "#196127"
-];
+const colors = [null, "#ebedf0", "#c6e48b", "#7bc96f", "#196127"];
 
 const test = new Command();
 test.name = "test";
-test.funct = async (bot:Guide, message, args) => {
+test.funct = async (bot: Guide, message, args) => {
   const pixels = bot.target_image;
 
-  const canvas = Canvas.createCanvas(Math.ceil(pixels.length/7)*11, 11*7);
+  const canvas = Canvas.createCanvas(Math.ceil(pixels.length / 7) * 11, 11 * 7);
   const ctx = canvas.getContext("2d");
 
   let x = 0;
   let y = 0;
-  for( let pixel of pixels ){
-    ctx.fillStyle = colors[pixel]
-    ctx.fillRect(x*11, y*11, 10, 10);
-    y++
-    if(y==7){
-      x++
-      y=0
+  for (let pixel of pixels) {
+    ctx.fillStyle = colors[pixel];
+    ctx.fillRect(x * 11, y * 11, 10, 10);
+    y++;
+    if (y == 7) {
+      x++;
+      y = 0;
     }
   }
 

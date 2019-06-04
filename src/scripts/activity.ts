593e0c6ba545261cd20fa3f1aa1a03a44c867720
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-06-04 17:22:24 +0200

    nani

--- src/scripts/activity.ts
+++ src/scripts/activity.ts
@@ -1,10 +1,10 @@
 import { Script } from "vnftjs";
-import { Guide } from "../classes/Guide.ts";
+import { Guide } from "../classes/Guide";
 
 const activity = new Script();
 
 activity.funct = async (client: Guide) => {
-  const done = await client.fetchMadeCommits();
+  const done = await client.fetchMadeCommits("VonFriedricht");
   const required = client.requiredCommits;
   const progress = `${done} / ${required}`;
   const nextword = await client.nextWords(1);

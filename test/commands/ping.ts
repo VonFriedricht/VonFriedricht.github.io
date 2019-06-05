0c3b8cd630a778e527fbcec603d33175e2b36666
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-06-05 17:00:19 +0200

    Hito

---
+++ test/commands/ping.ts
@@ -0,0 +1,12 @@
+import { Command } from "vnftjs";
+
+const ping = new Command();
+ping.name = "ping";
+
+ping.funct = (client, message, args) => {
+  message.reply("pongo!!");
+};
+
+ping.addUserWhitelist(u => u.id == "397063436049186818");
+
+module.exports = ping;

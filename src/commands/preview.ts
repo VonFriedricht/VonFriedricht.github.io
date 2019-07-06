7b50a59203c596de52065dee4c55046c24bad603
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-07-06 17:07:57 +0200

    [The]

--- src/commands/preview.ts
+++ src/commands/preview.ts
@@ -3,58 +3,54 @@ import { Attachment } from 'discord.js'
 import { Guide } from '../classes/Guide'
 
 const preview = new Command()
-preview.name = "preview"
-preview.addAlias("p")
+preview.name = 'preview'
+preview.addAlias('p')
 
-preview.funct = (bot, message, args) => {
+preview.funct = (bot: Guide, message, args) => {
   try {
     const Canvas = require('canvas')
 
-    preview.name = 'preview'
-    preview.addAlias('p')
-    preview.funct = async (bot: Guide, message, args) => {
-      const pixels = bot.targetImage
-      const colors = [null, '#ebedf0', '#c6e48b', '#7bc96f', '#196127']
+    const pixels = bot.targetImage
+    const colors = [null, '#ebedf0', '#c6e48b', '#7bc96f', '#196127']
 
-      const pixelSize = 12
-      const pixelMargin = 2
-      const pixelSpace = pixelSize + pixelMargin
-      const highlightSize = 2
+    const pixelSize = 12
+    const pixelMargin = 2
+    const pixelSpace = pixelSize + pixelMargin
+    const highlightSize = 2
 
-      const pixelRows = Math.ceil(pixels.length / 7)
+    const pixelRows = Math.ceil(pixels.length / 7)
 
-      const canvas = Canvas.createCanvas(pixelRows * pixelSpace, pixelSpace * 7)
-      const ctx = canvas.getContext('2d')
+    const canvas = Canvas.createCanvas(pixelRows * pixelSpace, pixelSpace * 7)
+    const ctx = canvas.getContext('2d')
 
-      let x = 0
-      let y = 0
-      let day = 0
-      for (let pixel of pixels) {
-        let pixelX = x * pixelSpace
-        let pixelY = y * pixelSpace
-        if (day == bot.day) {
-          ctx.fillStyle = 'red'
-          ctx.fillRect(
-            pixelX - highlightSize + pixelMargin / 2,
-            pixelY - highlightSize + pixelMargin / 2,
-            pixelSize + highlightSize * 2,
-            pixelSize + highlightSize * 2
-          )
-        }
-        ctx.fillStyle = colors[pixel]
-        ctx.fillRect(pixelX, pixelY, pixelSize, pixelSize)
-
-        y++
-        day++
-        if (y == 7) {
-          x++
-          y = 0
-        }
+    let x = 0
+    let y = 0
+    let day = 0
+    for (let pixel of pixels) {
+      let pixelX = x * pixelSpace
+      let pixelY = y * pixelSpace
+      if (day == bot.day) {
+        ctx.fillStyle = 'red'
+        ctx.fillRect(
+          pixelX - highlightSize + pixelMargin / 2,
+          pixelY - highlightSize + pixelMargin / 2,
+          pixelSize + highlightSize * 2,
+          pixelSize + highlightSize * 2
+        )
       }
+      ctx.fillStyle = colors[pixel]
+      ctx.fillRect(pixelX, pixelY, pixelSize, pixelSize)
 
-      const attachment = new Attachment(canvas.toBuffer(), `preview.png`)
-      message.channel.send(args, attachment)
+      y++
+      day++
+      if (y == 7) {
+        x++
+        y = 0
+      }
     }
+
+    const attachment = new Attachment(canvas.toBuffer(), `preview.png`)
+    message.channel.send(args, attachment)
   } catch (err) {
     console.log(err)
   }
--- src/commands/test.ts
+++ src/commands/test.ts
@@ -1,23 +1,22 @@
-import { Command } from "vnftjs";
+import { Command } from 'vnftjs'
 
 const reee = new Command()
 
-reee.name = "reee"
+reee.name = 'reee'
 
 reee.funct = (bot, message, args) => {
-    try{
-        const Canvas = require('canvas')
-        const { Attachment } = require('discord.js')
-        const canvas = Canvas.createCanvas(50, 50)
-        const ctx = canvas.getContext('2d')
-        ctx.fillRect(10,10,20,30)
-        
-        const attachment = new Attachment(canvas.toBuffer(), `reee.png`)
-        message.channel.send(" ", attachment)
-    }
-    catch(error){
-        message.reply(error.message)
-    }
+  try {
+    const Canvas = require('canvas')
+    const { Attachment } = require('discord.js')
+    const canvas = Canvas.createCanvas(50, 50)
+    const ctx = canvas.getContext('2d')
+    ctx.fillRect(10, 10, 20, 30)
+
+    const attachment = new Attachment(canvas.toBuffer(), `reee.png`)
+    message.channel.send(' ', attachment)
+  } catch (error) {
+    message.reply(error.message)
+  }
 }
 
 export = reee
